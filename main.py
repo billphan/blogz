68cf7a8e3c4a173fc83e89f5f86a9ab518e9c2a3
Bill Phan <billphan25@gmail.com>
2017-07-17 21:10:00 -0700

    Changed database connection string to match new database.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy import desc
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'xfd{H\xe5<\xf9\x6a2\xa0\x9fR"\xa1\xa8'
